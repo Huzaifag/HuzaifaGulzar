27751ca2f9573d99c046b9c0a818a5d8d36f6255
Huzaifag <92973781+Huzaifag@users.noreply.github.com>
2024-09-18 16:48:07 +0500

    Update Photo.jsx

--- components/ui/Photo.jsx
+++ components/ui/Photo.jsx
@@ -21,7 +21,7 @@ export default function Photo() {
         }}
         className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten absolute">
           <Image
-            src="Public/assets/Photo.png"
+            src="/assets/Photo.png"
             priority
             quality={100}
             fill
